ca62da58ce7d98bf8891ef89b869e7a5d9630af0
James A. <jalaniz1@mail.csuchico.edu>
2015-02-11 01:24:14 -0700

    Saving on github

---
+++ Source/MyProject2/HelloWorldPrinter.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "HelloWorldPrinter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT2_API AHelloWorldPrinter : public AActor
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
